57cd6be11b7bb6ed7ad2b3b32a2bad231a88e770
Carl van Heerden <carljvh@gmail.com>
2025-12-06 11:23:53 +0200

    Added Daisy UI headers, playing around with fasthtml

--- app/main.py
+++ app/main.py
@@ -1,11 +1,29 @@
 import os
 from fasthtml.common import *
+import fasthtml.components as fc
 
-app, rt = fast_app()
+daisy_headers = (
+    Link(href='https://cdn.jsdelivr.net/npm/daisyui@5', rel='stylesheet', type='text/css'),
+    Script(src='https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4')
+)
+
+def Button(*c, cls='', **kw):
+    return fc.Button(*c, cls=f"btn {cls}", **kw)
+
+app, rt = fast_app(title="carldotcom's playground", hdrs = daisy_headers
+    )
+
+@rt
+def btn_res(nm:str): return f"Button Clicked, hello {nm}!"
 
 @rt('/')
 def get(): 
-    return Div(P('Hello World!'), hx_get="/change")
+    return Form(
+        H1('Welcome to the playground!', cls='text-3xl'), 
+        Input(type='text', id='nm', placeholder='Enter your name'),
+        Button('Click me', hx_post="/btn_res", hx_target='#dest'),
+        P(id='dest', cls='mt-4')
+    )
 
 @rt('/health')
 def get():
